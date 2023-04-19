df28a8dbf8af271f297d5ffefd5b05d6f1891a85
trashkids <trashkids@gmail.com>
2023-04-19 22:26:58 +0900

    gpt4

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def generate_answer(text):
     prompt = f"ユーザーが次のように言っています: '{text}'。これに対する適切な返答は何ですか？"
     response = openai.Completion.create(
-      engine="text-davinci-003",
+#      engine="text-davinci-003",
+      model=gpt-4
       prompt=prompt,
       temperature=0.5,
       max_tokens=2000,
