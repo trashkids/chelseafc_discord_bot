dfa4035038eb339de9cc987a55351dfe6c81ec45
trashkids <trashkids@gmail.com>
2023-04-20 13:43:45 +0900

    fix

--- bot.py
+++ bot.py
@@ -20,33 +20,35 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_api_url = os.getenv('OPENAI_API_URL') or 'https://api.openai.com/v1/chat/completions'
 
-# GPT4にテキストを送り回答を返す関数
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {openai_api_key}',
+}
+
+# Function to send text to GPT-4 and return the answer
 def generate_answer(text):
-    prompt = f"ユーザーが次のように言っています: '{text}'。これに対する適切な返答は何ですか？"
     data = {
-        "engine": "gpt-4",
-        "prompt": prompt,
-        "temperature": 0.5,
-        "max_tokens": 2000,
-        "n": 1,
-        "timeout": 10,
+        'model': 'gpt-3.5-turbo',
+        'messages': [
+            {'role': 'system', 'content': 'あなたは若者で、カジュアルな話し方をします。敬語を使わずスラングを使います。'},
+            {'role': 'user', 'content': text}
+        ],
+        'temperature': 0.8,
+        'max_tokens': 500,
+        'n': 1,
     }
-    headers = {
-        "Authorization": f"Bearer {openai_api_key}",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post("https://api.openai.com/v2/completions", headers=headers, data=json.dumps(data))
+    
+    response = requests.post(openai_api_url, headers=headers, json=data)
     response_json = response.json()
-    answer = response_json['choices'][0]['text'].strip()
-
-    return answer
-
-# 日本語判定をする関数
-def is_japanese(text):
-    regex = r'[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf]+'
-    return bool(re.search(regex, text))
+    
+    if response.status_code == 200:
+        answer = response_json['choices'][0]['message']['content'].strip()
+        return answer
+    else:
+        logger.error(f"API error: {response_json}")
+        return "Error: Unable to generate a response."
 
 @client.event
 async def on_ready():
@@ -57,18 +59,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    text = message.content.replace(f'<@!{client.user.id}>', '').replace(f'<@{client.user.id}>', '').strip()
+    # Check if the bot is mentioned in the message
+    if client.user.mentioned_in(message):
+        text = message.content.replace(f'<@!{client.user.id}>', '').replace(f'<@{client.user.id}>', '').strip()
 
-    if not is_japanese(text):
+        logger.info(f"User input: {text}")
+        answer = generate_answer(text)
+        logger.info(f"API response: {answer}")
+
+        await message.channel.send(answer)
+        logger.info(f"Bot sent message: {answer}")
+    else:
+        # Do nothing if the bot is not mentioned
         return
 
-    logger.info(f"User input: {text}")
-    answer = generate_answer(text)
-    logger.info(f"API response: {answer}")
-
-    await message.channel.send(answer)
-    logger.info(f"Bot sent message: {answer}")
-
-# 環境変数を読み込む部分の修正
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 client.run(TOKEN)
